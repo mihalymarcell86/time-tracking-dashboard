3f32d03d5e0deb73241d8b7c14d0ec8b9ad7f816
Marcell Mihály <mihalymarcell86@gmail.com>
2022-07-30 20:17:10 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <>
+      Report for Jeremy Robson Daily Weekly Monthly Work 5hrs {/* daily */}
+      Previous - 7hrs {/* daily */}
+      32hrs {/* weekly */}
+      Previous - 36hrs {/* weekly */}
+      103hrs {/* monthly */}
+      Previous - 128hrs {/* monthly */}
+      Play 1hr {/* daily */}
+      Previous - 2hrs {/* daily */}
+      10hrs {/* weekly */}
+      Previous - 8hrs {/* weekly */}
+      23hrs {/* monthly */}
+      Previous - 29hrs {/* monthly */}
+      Study 0hrs {/* daily */}
+      Previous - 1hr {/* daily */}
+      4hrs {/* weekly */}
+      Previous - 7hrs {/* weekly */}
+      13hrs {/* monthly */}
+      Previous - 19hrs {/* monthly */}
+      Exercise 1hr {/* daily */}
+      Previous - 1hr {/* daily */}
+      4hrs {/* weekly */}
+      Previous - 5hrs {/* weekly */}
+      11hrs {/* monthly */}
+      Previous - 18hrs {/* monthly */}
+      Social 1hr {/* daily */}
+      Previous - 3hrs {/* daily */}
+      5hrs {/* weekly */}
+      Previous - 10hrs {/* weekly */}
+      21hrs {/* monthly */}
+      Previous - 23hrs {/* monthly */}
+      Self Care 0hrs {/* daily */}
+      Previous - 1hr {/* daily */}
+      2hrs {/* weekly */}
+      Previous - 2hrs {/* weekly */}
+      7hrs {/* monthly */}
+      Previous - 11hrs {/* monthly */}
+      <div class="attribution">
+        Challenge by{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://www.frontendmentor.io?ref=challenge"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer"
         >
-          Learn React
+          Frontend Mentor
         </a>
-      </header>
-    </div>
+        . Coded by{" "}
+        <a
+          href="https://www.github.com/mihalymarcell86"
+          target="_black"
+          rel="noreferrer"
+        >
+          Your Name Here
+        </a>
+        .
+      </div>
+    </>
   );
 }
 
